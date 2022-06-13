e09d1f15953392caeb8c54b064594bde9e981cb8
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-13 22:42:06 +0500

    It's Time To Push !

--- Node JS/REST-API-03/src/db/conc.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const URI = "mongodb+srv://syed_bilal:1234@cluster0.wwm7o.mongodb.net/mern?retryWrites=true&w=majority"  
-
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }).then(() => {
-    console.log('connection successful')
-}).catch((err) => {
-    console.log(err);
-});
