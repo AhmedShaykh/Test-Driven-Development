bdd0c4b7a85139961a8a47894e520d24ce347eb5
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-14 20:31:19 +0500

    It's Time To Push !

--- NodeJS/02_read_write_file.js
+++ NodeJS/02_read_write_file.js
@@ -2,23 +2,25 @@ var fs = require('fs');
 
 function readFile(srcPath) {
     fs.readFile(srcPath, 'utf8', function (err, data) {
-        (err) ? console.log('Read Err ', err) : console.log('Read ', data);
+        (err) ? console.log('Read Err ', err) : console.log('Read ==>', data);
     });
 }
+
 function writeFile(savPath, data) {
     fs.writeFile(savPath, data, function (err) {
         (err) ? console.log('Write Err: ', err) : console.log('Write Successfully!!');
     });
 }
+
 function appendFile(savPath, data) {
     fs.appendFile(savPath, data, function (err) {
         (err) ? console.log('Append Err: ', err) : console.log('Append Successfully!!');
     });
 }
 
-writeFile('./abc.txt', 'Yahoo! My First Data Save In File...');
-// readFile('./abc.txt');
-// appendFile('./abc.txt', 'Abc Yhaoo My First File... 123');
+// writeFile('./abc.txt', 'Yahoo! My First Data Save In File ');
+readFile('./abc.txt');
+appendFile('./abc.txt', 'With Node.JS!');
 
 // function readFile(srcPath) {
 //     return new Promise(function (resolve, reject) {
