8f2603556ef4b90bd0671ba7baa098f8179b7c7a
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-14 14:02:49 +0500

    It's Time To Push !

--- NodeJS/02_filesystem.js
+++ NodeJS/02_filesystem.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 
 // ** SYNC ** //
-// console.log('SYNC 1 ');
+console.log('SYNC 1 ');
 
-// var files = fs.readdirSync('./');  // I/O BLOCK
-// console.log('SYNC 2', files);
+var files = fs.readdirSync('./');  // I/O BLOCK
+console.log('SYNC 2', files);
 
 // ** ASYNC ** //
 console.log('ASYNC 1');
 
 fs.readdir('./', function (err, files) {
     if (err) {
-        console.log('Err: ', err)
+        console.log('Error: ', err)
     } else {
         console.log('Files: ', files);
     }
