a843a0dbbe6801112a3a72afd1159e935b5820aa
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-14 21:25:47 +0500

    It's Time To Push !

--- NodeJS/03_events.js
+++ NodeJS/03_events.js
@@ -1,23 +1,14 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
-// What is Class....
-// Class: HUMAN (BLUE PRINT)
-// OBJECT: JHON (ACTUAL OBJECT)
-
-
-// Register a Listener
 emitter.on('messageLogged', function (arg) {
     console.log('Listener Called', arg);
 });
 
+console.log('Emitting....')
+emitter.emit('messageLogged', { id: 1, url: 'http://myUrl' });
 
-// Raise Event...
-console.log('Emiting....')
-emitter.emit('messageLogged', {id: 1, url: 'http://myUrl'});        // EMIT: Making a noise, produce - signalling
-
-// Event Fire after 1000ms
 setTimeout(() => {
-    console.log('Emiting from setTimeout');
-    emitter.emit('messageLogged', {id: 2, from: 'setTimeout'});        // EMIT: Making a noise, produce - signalling
+    console.log('Emitting from setTimeout');
+    emitter.emit('messageLogged', { id: 2, from: 'setTimeout' });
 }, 2000);
