a03aa39092cc4f22cf910d4a9aa7a00e84be0fb9
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-15 15:44:46 +0500

    It's Time To Push !

--- NodeJS/07_url.js
+++ NodeJS/07_url.js
@@ -4,9 +4,12 @@ var url = require('url');
 http.createServer(function (req, res) {
   console.log('URl ', req.url);
   console.log('MEthod', req.method);
+
   res.writeHead(200, {'Content-Type': 'text/html'});
   var q = url.parse(req.url, true).query;
   console.log('q ', q);
+
   var txt = q.year + " " + q.month;
   res.end(txt);
-}).listen(8080);
+})
+.listen(8080);
